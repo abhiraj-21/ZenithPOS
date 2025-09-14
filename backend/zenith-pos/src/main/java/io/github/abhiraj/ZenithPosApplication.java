e90c83cf59094898714360f8a02b7c155e9d48aa
Abhiraj <abhirajsinghchouhan4@gmail.com>
2025-09-14 11:29:58 +0530

    backend project structure

---
+++ backend/zenith-pos/src/main/java/io/github/abhiraj/ZenithPosApplication.java
@@ -0,0 +1,13 @@
+package io.github.abhiraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenithPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenithPosApplication.class, args);
+	}
+
+}
